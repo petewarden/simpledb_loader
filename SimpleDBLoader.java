fd3afb192e1ccfa8c02f8f8816f33f0beea0ec9c
Pete Warden <pete@petewarden.com>
2010-01-23 18:40:15 -0700

    Implemented JSON file loading, adding command-line arguments for configuration

--- SimpleDBLoader.java
+++ SimpleDBLoader.java
@@ -9,11 +9,21 @@
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
 import com.amazonaws.sdb.*;
 import com.amazonaws.sdb.model.*;
 import java.util.concurrent.Future;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.zip.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.*;
 
 class ItemToAdd {
     public ReplaceableItem item;
@@ -91,6 +101,133 @@ class TestItemGenerator implements ItemGenerator {
     }
 }
 
+// Loads data from a file, consisting of lines starting with a string for the id, 
+// followed by a tab, then a JSON string representing the item's attributes
+class JSONFileItemGenerator implements ItemGenerator {
+
+    BufferedReader reader;
+    int domainCount;
+    
+    public JSONFileItemGenerator(String filename, int inDomainCount) 
+    {
+        try 
+        {
+            FileInputStream inputStream = new FileInputStream(filename);
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+        }
+        catch (FileNotFoundException exception)
+        {
+            System.err.println("File could not be read: "+filename);
+            System.exit(1);
+        }
+
+        domainCount = inDomainCount;
+    }
+    
+    public boolean hasMoreItems()
+    {
+        try
+        {
+            return reader.ready();
+        }
+        catch (IOException exception)
+        {
+            return false;
+        }
+    }
+    
+    public int hashFunction(String key)
+    {
+        CRC32 crc = new CRC32();
+        crc.update(key.getBytes());
+        return (int)(crc.getValue());
+    }
+    
+    public int getDomainForKey(String key)
+    {
+        int hash = hashFunction(key);
+        return (Math.abs(hash)%domainCount);
+    }
+    
+    public ItemToAdd getNextItem()
+    {
+        String line = "";
+        try 
+        {
+            line = reader.readLine();
+        }
+        catch (IOException exception)
+        {
+            System.err.println("Error reading from file");
+            System.exit(1);
+        }
+
+        String [] parts = line.split("\t", 2);
+            
+        String key;
+        String dataString;
+        if (parts.length<2)
+        {
+            System.err.println("Bad line found:"+line);
+            key = line;
+            dataString = "{}";
+        }
+        else 
+        {
+            key = parts[0];
+            dataString = parts[1];
+        }
+
+        JSONObject data = (JSONObject)(JSONValue.parse(dataString));
+
+        int domainIndex = getDomainForKey(key);
+
+        ArrayList<ReplaceableAttribute> attributes = new ArrayList<ReplaceableAttribute>();
+
+        Iterator it = data.entrySet().iterator();
+        while (it.hasNext()) 
+        {
+            Map.Entry pairs = (Map.Entry)it.next();
+
+            String baseName = pairs.getKey().toString();
+            String value = JSONValue.toJSONString(pairs.getValue());
+
+            final int maxLength = 1020;
+
+            if (value.length()<maxLength)
+            {
+                attributes.add(new ReplaceableAttribute(baseName, value, false));
+            }
+            else 
+            {
+                int index = 0;
+                while (value.length()>0)
+                {
+                    String subValue = value.substring(0, Math.min(maxLength, value.length()));
+                    String name;
+                    if (index==0)
+                        name = baseName;
+                    else
+                        name = baseName+"*"+Integer.toString(index);
+                    
+                    attributes.add(new ReplaceableAttribute(name, subValue, false));
+            
+                    value = value.substring(Math.min(maxLength, value.length()));
+                    index += 1;
+                }
+
+                
+            }
+
+        }            
+        ReplaceableItem item = new ReplaceableItem(key, attributes);
+        
+        ItemToAdd result = new ItemToAdd(item, domainIndex);
+        
+        return result;    
+    }
+}
+
 // Needed to prevent warnings on a call to the clone method on a generic ArrayList
 @SuppressWarnings("unchecked")
 public class SimpleDBLoader {
@@ -98,7 +235,8 @@ public class SimpleDBLoader {
     /************************************************************************
     * Access Key ID and Secret Access Key ID, obtained from:
     * http://aws.amazon.com
-    * You need to set this to your own keys before running this example
+    * You need to set this to your own keys before running this example,
+    * or supply your own credentials on the command line
     ***********************************************************************/
     public String accessKeyId = "";
     public String secretAccessKey = "";
@@ -106,6 +244,13 @@ public class SimpleDBLoader {
     int domainCount = 25;
     String domainPrefix = "test_domain";
     int batchCount = 20;
+    int threadCount = 100;
+    float minRPS = 1.0f;
+    float maxRPS = 3.0f;
+    long rampTime = (120*1000);
+
+    int itemCount = 10000;
+    String filename = "";
 
     ArrayList<ArrayList<ReplaceableItem>> domainItems = new ArrayList<ArrayList<ReplaceableItem>>(domainCount);
     List<Future<BatchPutAttributesResponse>> putResponses = new ArrayList<Future<BatchPutAttributesResponse>>();
@@ -116,27 +261,27 @@ public class SimpleDBLoader {
     public static void main(String[] args) {
 
         SimpleDBLoader loader = new SimpleDBLoader();
+        
+        String command = loader.parseArguments(args);
+
+        if (command.equals("help"))
+        {
+            loader.printHelp();
+            System.exit(1);
+        }
+        
         if (loader.accessKeyId.equals(""))
         {
-            System.out.println("*************************************************************************************************************");
-            System.out.println("*You need to edit SimpleDBLoader.java to add your own AWS access keys and recompile before you can run this.*");
-            System.out.println("*************************************************************************************************************");
+            System.out.println("*****************************************************************************************");
+            System.out.println("*You need to supply your access keys vias the command line, or edit SimpleDBLoader.java*");
+            System.out.println("*to add your own AWS access keys and recompile before you can run this.                *");
+            System.out.println("****************************************************************************************");
             System.out.println("");
             loader.printHelp();
             System.exit(1);
         }
         
-        String command;
-        if (args.length<1)
-            command = "help";
-        else 
-            command = args[0];
-
-        if (command.equals("help"))
-        {
-            loader.printHelp();
-        }
-        else if (command.equals("setup"))
+        if (command.equals("setup"))
         {
             loader.setup();
         }
@@ -146,31 +291,17 @@ public class SimpleDBLoader {
         }
         else if (command.equals("test"))
         {
-            int itemCount = 10000;
-            if (args.length>1)
+            loader.runTests();
+        }
+        else if (command.equals("loadjson"))
+        {
+            if (loader.filename.equals(""))
             {
-                try {
-                    itemCount = Integer.parseInt(args[1]);
-                } catch (NumberFormatException e) {
-                    System.out.println("Test item count argument must be an integer");
-                    loader.printHelp();
-                    System.exit(1);
-                }
+                System.out.println("You must supply a filename to load the data from.");
+                loader.printHelp();
+                System.exit(1);
             }
-
-            int threadCount = 100;
-            if (args.length>2)
-            {
-                try {
-                    threadCount = Integer.parseInt(args[2]);
-                } catch (NumberFormatException e) {
-                    System.out.println("Test item count argument must be an integer");
-                    loader.printHelp();
-                    System.exit(1);
-                }
-            }
-            
-            loader.runTests(itemCount, threadCount);
+            loader.loadJSONFile();
         }
         else 
         {
@@ -179,19 +310,141 @@ public class SimpleDBLoader {
         }
     }
     
+    public String parseArguments(String[] args)
+    {
+        String command;
+        if (args.length<1)
+            command = "help";
+        else 
+            command = args[0].toLowerCase();
+        
+        if (command.equals("help"))
+            return command;
+            
+        for (int index=1; index<args.length;)
+        {
+            String currentToken = args[index];
+            index += 1;
+            
+            if (!currentToken.startsWith("-"))
+            {
+                System.out.println("Unexpected argument: "+currentToken);
+                printHelp();
+                System.exit(1);
+            }
+        
+            String fullArgName = currentToken.substring(1);
+            String [] argParts = fullArgName.split("=", 2);
+
+            String argName;
+            String argValue;
+            if (argParts.length<2)
+            {
+                argName = fullArgName;
+                
+                if (index>=args.length)
+                {
+                    System.out.println("Missing value for argument: "+currentToken);
+                    printHelp();
+                    System.exit(1);
+                }
+                
+                argValue = args[index];
+                index += 1;
+            }
+            else 
+            {
+                argName = argParts[0];
+                argValue = argParts[1];
+            }
+
+            if (argName.equals("domaincount")||argName.equals("d"))
+            {
+                domainCount = Integer.parseInt(argValue);
+            }
+            else if (argName.equals("domainprefix")||argName.equals("p"))
+            {
+                domainPrefix = argValue;
+            }
+            else if (argName.equals("threadcount")||argName.equals("t"))
+            {
+                threadCount = Integer.parseInt(argValue);
+            }            
+            else if (argName.equals("itemcount")||argName.equals("i"))
+            {
+                itemCount = Integer.parseInt(argValue);
+            }
+            else if (argName.equals("batchcount")||argName.equals("b"))
+            {
+                batchCount = Integer.parseInt(argValue);
+            }
+            else if (argName.equals("filename")||argName.equals("f"))
+            {
+                filename = argValue;
+            }
+            else if (argName.equals("maxrps")||argName.equals("m"))
+            {
+                maxRPS = Float.parseFloat(argValue);
+            }
+            else if (argName.equals("minrps")||argName.equals("n"))
+            {
+                maxRPS = Float.parseFloat(argValue);
+            }
+            else if (argName.equals("ramptime")||argName.equals("r"))
+            {
+                rampTime = Integer.parseInt(argValue)*1000;
+            }
+            else if (argName.equals("accesskey")||argName.equals("a"))
+            {
+                accessKeyId = argValue;
+            }            
+            else if (argName.equals("secretkey")||argName.equals("s"))
+            {
+                secretAccessKey = argValue;
+            }            
+            else
+            {
+                System.out.println("Unexpected argument: "+currentToken);
+                printHelp();
+                System.exit(1);
+            }
+        
+        }
+        
+        return command;
+    }
+    
     public void printHelp()
     {
+        System.out.println("");
         System.out.println("This project explores the fastest way of loading large amounts of data into SimpleDB.");
         System.out.println("");
-        System.out.println("You'll need to edit SimpleDBLoader.java to add your own AWS keys and recompile before you can do anything.");
-        System.out.println("Once you've done that, create the domains you'll need by running this script as:");
-        System.out.println("java -cp<...> SimpleDBLoader setup");
+        System.out.println("First, create the domains you'll need by running this script as:");
+        System.out.println("./sdbloader setup");
         System.out.println("");
-        System.out.println("After that, you can test the data loading speed by passing 'test' as the first argument, followed by an optional number of items (defaults to 10000)");
-        System.out.println("and the number of threads to use (defaults to 100)");
+        System.out.println("After that, you can test the data loading speed by passing 'test' as the first argument");
         System.out.println("When you're finished testing, run this command to remove the test domains:");
-        System.out.println("java -cp<...> SimpleDBLoader cleanup");
+        System.out.println("./sdbloader cleanup");
         System.out.println("");
+        System.out.println("This class also functions as a fast way to upload data from files. You can pass in 'loadjson' as the first argument,");
+        System.out.println("followed by -filename/-f <filename>, and the file's data will be uploaded.");
+        System.out.println("The JSON format for uploading is to have each item's data on a separate line in the file. The first string is the unique ID to use");
+        System.out.println("as the item's key, followed by a tab, then a JSON string containing an associative array describing that item's attributes and values.");
+        System.out.println("");
+        System.out.println("You can supply the following arguments to configure the loading:");
+        System.out.println("-domaincount/-d : How many domains to shard the items across (defaults to 25)");
+        System.out.println("-domainprefix/-p : The string to start all the domain names with, followed by a number for each (defaults to 'test_domain')");
+        System.out.println("-threadcount/-t : The number of threads to run in parallel (defaults to 100)");
+        System.out.println("-itemcount/-i : For the 'test' benchmarking command, sets the number of items to generate and load (defaults to 10000)");
+        System.out.println("-batchcount/-b : How many items to batch together into a single HTTP request to SimpleDB (defaults to 20)");
+        System.out.println("-filename/-f : For data loading commands, the filename to read the data from");
+        System.out.println("-minrps/-n : To avoid throttling, we need to slowly ramp up our request speed. This is the number of requests per-domain, per-second to start with (defaults to 1.0)");
+        System.out.println("-maxrps/-m : The maximum number of requests per-domain, per-second to ramp up to with (defaults to 3.0)");
+        System.out.println("-ramptime/-r : How long to ramp up to the maximum rate, in seconds (defaults to 120)");
+        System.out.println("-accesskey/-a : Your AWS account access key, required");
+        System.out.println("-secretkey/-s : Your AWS secret key, required");
+        System.out.println("");
+        
         System.out.println("By Pete Warden <pete@petewarden.com> - see http://petewarden.typepad.com/ for more details");
     }
     
@@ -203,7 +456,7 @@ public class SimpleDBLoader {
             return domainPrefix+index;
     }
     
-    public AmazonSimpleDBConfig getConfig(int threadCount)
+    public AmazonSimpleDBConfig getConfig()
     {
         AmazonSimpleDBConfig config = new AmazonSimpleDBConfig().withMaxConnections(threadCount);
         config.setMaxErrorRetry(5);
@@ -213,9 +466,9 @@ public class SimpleDBLoader {
     
     public void setup()
     {
-        AmazonSimpleDBConfig config = getConfig(100);
+        AmazonSimpleDBConfig config = getConfig();
         
-        ExecutorService executor = Executors.newFixedThreadPool(100);
+        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
         AmazonSimpleDBAsync service = new AmazonSimpleDBAsyncClient(accessKeyId, secretAccessKey, config, executor);
 
         List<CreateDomainRequest> requests = new ArrayList<CreateDomainRequest>();
@@ -233,9 +486,9 @@ public class SimpleDBLoader {
     
     public void cleanup()
     {
-        AmazonSimpleDBConfig config = getConfig(100);
+        AmazonSimpleDBConfig config = getConfig();
 
-        ExecutorService executor = Executors.newFixedThreadPool(100);
+        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
         AmazonSimpleDBAsync service = new AmazonSimpleDBAsyncClient(accessKeyId, secretAccessKey, config, executor);
 
         List<DeleteDomainRequest> requests = new ArrayList<DeleteDomainRequest>();
@@ -259,13 +512,13 @@ public class SimpleDBLoader {
         long currentTime = (System.currentTimeMillis()-jobStartTime);
         
         // Ramp up over 120 seconds
-        float maxTime = (120*1000);
+        float maxTime = rampTime;
         
         // Start with one request per second
-        float startValue = 1.0f;
+        float startValue = minRPS;
         
         // And finish with four requests per second
-        float endValue = 3.0f;
+        float endValue = maxRPS;
         
         float currentRPS;
         if (currentTime>maxTime)
@@ -320,16 +573,23 @@ public class SimpleDBLoader {
         domainItems.set(domainIndex, new ArrayList<ReplaceableItem>());    
     }
 
-    public void runTests(int itemCount, int threadCount) 
+    public void runTests() 
     {
         ItemGenerator generator = new TestItemGenerator(itemCount, domainCount);
         
-        runLoading(generator, threadCount);
+        runLoading(generator);
     }
     
-    public void runLoading(ItemGenerator generator, int threadCount)
+    public void loadJSONFile()
     {
-        AmazonSimpleDBConfig config = getConfig(threadCount);
+        ItemGenerator generator = new JSONFileItemGenerator(filename, domainCount);
+        
+        runLoading(generator);
+    }
+    
+    public void runLoading(ItemGenerator generator)
+    {
+        AmazonSimpleDBConfig config = getConfig();
         ExecutorService executor = Executors.newFixedThreadPool(threadCount);
         AmazonSimpleDBAsync service = new AmazonSimpleDBAsyncClient(accessKeyId, secretAccessKey, config, executor);
     
